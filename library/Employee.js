9bb402f489bd6740a4680254dc4c6fc71a1307c8
rudy gauna <rudy.gauna@gmail.com>
2020-06-25 23:49:36 -0500

    had many major code and file restructuring, but finally figured it out

---
+++ library/Employee.js
@@ -0,0 +1,29 @@
+class Employee {
+  
+  constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  };
+
+  //returns the name
+  getName() {
+    return this.name;
+  };
+
+  //returns the id
+  getId() {
+    return this.id;
+  };
+  
+  //returns the email
+  getEmail() {
+    return this.email;
+  };
+  //returns the role or title
+  getRole() {
+    return "Employee";
+  };
+};
+
+module.exports = Employee;
